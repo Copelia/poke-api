6967818154a462396a6e02510b4bd5a1f96c1307
[Copelia] <iamcorallium@gmail.com>
2018-09-13 14:43:30 -0500

    Probando con loader, no carga : (

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-       //Eventos
+//Eventos
+       const $loader = document.getElementById('loader');
+
+       const buscarPokemon = async (event) => {
+        event.preventDefault();
+        $loader.classList.remove('hidden');
+        $pokeimg.style.display = 'none';
+        // event.currentTarget === $form
+        // Para enviar datos diferente a GET o POST. Accede a todo lo que está dentro de form
+        // También puedes settear datos cuando los usuarios no los ingresan explícitamente 
+        const form = new FormData($form);
+        const pokemonName = form.get('pokemon');
+        // console.log(pokemonName);
+        const pokemonData = await getPokemon(pokemonName);
+        buildPokemon(pokemonData, $poke, $pokeimg, $loader);
+      }
+    
+      const $form = document.getElementById('form');
+    $form.addEventListener('submit', buscarPokemon);    
+
        const $poke = document.getElementById('pokemon');
        const $pokeimg = document.getElementById('poke-img');
-       const $loader = document.getElementById('loader');
                 
        const buildPokemon = (pokemon, $nameContainer, $imgContainer, $loader) => {
            $loader.classList.add('hidden');
@@ -16,22 +34,7 @@
                  return pokemon;
            }
                             
-               const buscarPokemon = async (event) => {
-               event.preventDefault();
-               $loader.classList.remove('hidden');
-               $pokeimg.style.display('none');
-               // event.currentTarget === $form
-               // Para enviar datos diferente a GET o POST. Accede a todo lo que está dentro de form
-               // También puedes settear datos cuando los usuarios no los ingresan explícitamente 
-               const form = new FormData($form);
-               const pokemonName = form.get('pokemon');
-               // console.log(pokemonName);
-               const pokemonData = await getPokemon(pokemonName);
-               buildPokemon(pokemonData, $poke, $pokeimg)
-             }
-           
-             const $form = document.getElementById('form');
-           $form.addEventListener('submit', buscarPokemon);    
+              
    
            //Fetch es el API del browser que nos permite interactuar con datos
            //Refactorizando con async await
